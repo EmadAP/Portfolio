f96a0ea34b724f301913dc5aa81b6ab14c9d9341
EmadAP <emad.amoupour1997@gmail.com>
2025-10-23 00:45:29 +0330

    mouse animation responsive made

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
-import Mouse from "./components/Mouse.vue";
+import { ref, onMounted } from "vue";
 import ScrollToTopBtn from "./components/ScrollToTopBtn.vue";
 import Navbar from "./template/Navbar.vue";
+import Mouse from "./components/Mouse.vue";
+
+const isDesktop = ref(false);
+
+onMounted(() => {
+  isDesktop.value = window.matchMedia("(pointer: fine)").matches;
+});
 </script>
 
 <template>
   <div>
     <Navbar />
-
     <router-view />
-    <Mouse />
+
+    <Mouse v-if="isDesktop" />
+
     <ScrollToTopBtn />
   </div>
 </template>
